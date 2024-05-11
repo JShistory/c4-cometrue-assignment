63ee8a937cd124eb9747bf73dd5877e05ea69a5f
JShistory <behappy0916@naver.com>
2024-05-11 11:19:15 +0900

    feat: add User Role

--- src/main/java/org/c4marathon/assignment/openMarket/domain/User.java
+++ src/main/java/org/c4marathon/assignment/openMarket/domain/User.java
@@ -1,6 +1,8 @@
 package org.c4marathon.assignment.openMarket.domain;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -13,11 +15,13 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String account;
+    private String accountId;
     private String password;
     private String name;
     private String nickName;
     private String phoneNumber;
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
     @CreatedDate
     private LocalDateTime create;
     @LastModifiedDate
---
+++ src/main/java/org/c4marathon/assignment/openMarket/domain/UserRole.java
@@ -0,0 +1,5 @@
+package org.c4marathon.assignment.openMarket.domain;
+
+public enum UserRole {
+    USER, ADMIN, SELLER
+}
