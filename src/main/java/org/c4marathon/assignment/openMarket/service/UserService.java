58c86d99413b15dba75bf21049d01162b435f6cf
JShistory <behappy0916@naver.com>
2024-05-12 00:53:42 +0900

    feat: add User exception

--- src/main/java/org/c4marathon/assignment/openMarket/service/UserService.java
+++ src/main/java/org/c4marathon/assignment/openMarket/service/UserService.java
@@ -1,5 +1,7 @@
 package org.c4marathon.assignment.openMarket.service;
 
+import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.c4marathon.assignment.openMarket.domain.User;
 import org.c4marathon.assignment.openMarket.repository.UserRepository;
@@ -14,7 +16,16 @@ public class UserService {
 
     @Transactional
     public Long join(User user){
+        validateDuplicateMember(user);
         userRepository.save(user);
         return user.getId();
+    }
+
+    private void validateDuplicateMember(User user) {
+        //Exception
+        Optional<User> findUser = userRepository.findByAccountId(user.getAccountId());
+        if (findUser.isPresent()){
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
     }
 }
