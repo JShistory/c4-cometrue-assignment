6e25da6f10f7db8cf1db52d0e33b3d1320bc8280
JShistory <behappy0916@naver.com>
2024-05-14 11:10:48 +0900

    feat: api/v1/users에 보안 없이 접근 허용

--- src/main/java/org/c4marathon/assignment/openMarket/config/SecurityConfig.java
+++ src/main/java/org/c4marathon/assignment/openMarket/config/SecurityConfig.java
@@ -2,9 +2,14 @@ package org.c4marathon.assignment.openMarket.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
@@ -13,5 +18,17 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder getPasswordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .httpBasic(HttpBasicConfigurer::disable)
+                .csrf(CsrfConfigurer::disable)
+                .cors(Customizer.withDefaults())
+                .authorizeRequests()
+                .requestMatchers( "/api/v1/users").permitAll()
+                .and()
+                .build();
     }
 }
