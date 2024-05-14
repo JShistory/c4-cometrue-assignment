fd71e9697e3a24a88b4d9260911bd9caae136bed
JShistory <behappy0916@naver.com>
2024-05-14 17:47:49 +0900

    feat: item 저장, 조회 기능 controller 추가

--- src/main/java/org/c4marathon/assignment/openMarket/controller/UserApiController.java
+++ src/main/java/org/c4marathon/assignment/openMarket/controller/UserApiController.java
@@ -2,8 +2,11 @@ package org.c4marathon.assignment.openMarket.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.c4marathon.assignment.openMarket.domain.Item;
 import org.c4marathon.assignment.openMarket.domain.User;
+import org.c4marathon.assignment.openMarket.dto.ItemDTO;
 import org.c4marathon.assignment.openMarket.dto.UserDTO;
+import org.c4marathon.assignment.openMarket.service.ItemService;
 import org.c4marathon.assignment.openMarket.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +18,7 @@ import java.util.List;
 @RestController
 public class UserApiController {
     private final UserService userService;
+    private final ItemService itemService;
     @PostMapping("/users")
     public ResponseEntity createUser(@RequestBody @Valid UserDTO user){
         Long userId = userService.join(user);
@@ -28,5 +32,25 @@ public class UserApiController {
         UserDTO user = userService.findOne(id);
         return ResponseEntity.ok()
                 .body(user);
+    }
+
+    @PostMapping("/users/{id}/items")
+    public ResponseEntity createItem(@RequestBody @Valid ItemDTO item, @PathVariable("id") Long id){
+        UserDTO user = userService.findOne(id);
+        Long saveItem = itemService.saveItem(item, user);
+        return ResponseEntity.ok()
+                .body(saveItem);
+    }
+    @GetMapping("/users/{id}/items/{item_id}")
+    public ResponseEntity createItem(@PathVariable("id") Long id, @PathVariable("item_id") Long itemId){
+        UserDTO user = userService.findOne(id);
+        List<Item> items = user.getItems();
+        for(Item data : items){
+            if(data.getId().equals(itemId)){
+                return ResponseEntity.ok()
+                        .body(data);
+            }
+        }
+        return ResponseEntity.notFound().build();
     }
 }
