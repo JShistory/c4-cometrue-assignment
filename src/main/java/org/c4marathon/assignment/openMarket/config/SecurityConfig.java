4e979848052d404c5e739d8a38593af62f5ba46d
JShistory <behappy0916@naver.com>
2024-05-14 11:04:22 +0900

    feat: springboot security 적용

--- src/main/java/org/c4marathon/assignment/openMarket/config/SecurityConfig.java
+++ src/main/java/org/c4marathon/assignment/openMarket/config/SecurityConfig.java
@@ -2,9 +2,11 @@ package org.c4marathon.assignment.openMarket.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@EnableWebSecurity
 @Configuration
 public class SecurityConfig {
 
