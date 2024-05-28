2daecfb887305143cb2373932579500e3550906e
JShistory <behappy0916@naver.com>
2024-05-28 21:26:24 +0900

    init

--- frontend/vue.config.js
+++
@@ -1,8 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  outputDir: '../src/main/resources/static',
-    devServer: {
-      proxy: 'http://localhost'
-    }
-})
